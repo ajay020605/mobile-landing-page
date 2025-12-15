7a92e9c2077176986e77d03050d14aa4ff82bcfe
Ajay Yadav <ajy020605@gmail.com>
2025-12-16 01:25:05 +0530

    Update product descriptions for mobile devices

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -4,7 +4,7 @@ const mobiles = [
   {
     id: 1,
     name: "iPhone 15 Pro",
-    description: "Powerful performance with elegant design",
+    description: "Performance with elegant design",
     price: 1199,
     img: "https://media-ik.croma.com/prod/https://media.tatacroma.com/Croma%20Assets/Communication/Mobiles/Images/300823_0_qhq6qz.png",
   },
@@ -25,7 +25,7 @@ const mobiles = [
   {
     id: 4,
     name: "Pixel 8 Pro",
-    description: "Pure Android experience with AI camera",
+    description: "Pure Android experience",
     price: 899,
     img: "https://media-ik.croma.com/prod/https://media.tatacroma.com/Croma%20Assets/Communication/Mobiles/Images/309134_0_cv9vxa.png?tr=w-600",
   },
@@ -33,7 +33,7 @@ const mobiles = [
   {
   id: 5,
   name: "POCO X7",
-  description: "Minimal design with powerful performance",
+  description: "Minimal design with performance",
   price: 749,
   img: "https://i02.appmifile.com/570_operatorx_operatorx_opx/09/01/2025/0adbdc473ff18bd313ae2278a0d961e3.png",
 },
@@ -47,14 +47,14 @@ const mobiles = [
 {
   id: 7,
   name: "Realme GT 6",
-  description: "High performance at an aggressive price",
+  description: "High performance at an low price",
   price: 599,
   img: "https://img-prd-pim.poorvika.com/product/realme-gt-6t-5g-razor-green-256gb-12gb-ram-front-view.png",
 },
 {
   id: 8,
   name: "iQOO 12",
-  description: "Gaming powerhouse with flagship specs",
+  description: "Gaming powerhouse with fast charging",
   price: 699,
   img: "https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1701419586053/c26ca793c17f539c6d1dfb10b5b9d586.png",
 },
